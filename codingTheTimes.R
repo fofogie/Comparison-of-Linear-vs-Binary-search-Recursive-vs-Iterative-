f46d72c961bc2cdfeebc60dc644f913fabf7acb2
Farrell.J.Nusah <74714490+fofogie@users.noreply.github.com>
2026-01-03 17:23:54 +0700

    Refactor R script for search performance graphs

--- codingTheTimes.R
+++ codingTheTimes.R
@@ -4,7 +4,7 @@ options(scipen = 999)
 library(ggplot2)
 library(scales) 
 
-# Input data (Manual input Because CSV'S cant do anything )
+# Input data (Manual input Because CSV'S cant do anything...Will fix later )
 input_size <- c(10, 100, 500, 1000, 2500, 5000, 7500, 10000)
 lin_iter <- c(0.000533, 0.001300, 0.007067, 0.013933, 0.029700, 0.037300, 0.066167, 0.090300)
 lin_recur <- c(0.000400, 0.002100, 0.031800, 0.054233, 0.133267, 0.110300, 0.195667, 0.238733)
@@ -38,3 +38,4 @@ ggplot(df) +
   scale_color_manual(values = c("Iterative" = "darkgreen", "Recursive" = "orange"))
 
 #Im loosing myself.....
+
